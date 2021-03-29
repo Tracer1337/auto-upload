232b87c5e94950ec20ea994aa3c3843d3c5f12da
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-29 18:29:03 +0200

    Add register action

---
+++ actions/register.js
@@ -0,0 +1,26 @@
+const faker = require("faker")
+const config = require("../config.json")
+
+const REGISTER_URL = "https://cloud2go.tk/register"
+const EMAIL_SELECTOR = "#email"
+const PASSWORD_SELECTOR = "#register_password"
+const PASSWORD_CONFIRM_SELECTOR = "#register_password_confirmation"
+const SUBMIT_BUTTON_SELECTOR = "form button"
+
+async function register(browser) {
+    const page = await browser.newPage()
+    await page.goto(REGISTER_URL)
+    
+    const email = faker.internet.email()
+
+    await page.type(EMAIL_SELECTOR, email)
+    await page.type(PASSWORD_SELECTOR, config.password)
+    await page.type(PASSWORD_CONFIRM_SELECTOR, config.password)
+    await page.click(SUBMIT_BUTTON_SELECTOR)
+
+    await page.close()
+
+    return [email, config.password]
+}
+
+module.exports = register
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { events } = require("./utils")
 const generateFile = require("./actions/generateFile.js")
 const startBrowser = require("./actions/startBrowser.js")
+const register = require("./actions/register.js")
 
 async function run() {
     events.start()
@@ -13,6 +14,8 @@ async function run() {
     const browser = await startBrowser()
 
     // Register new account
+    const [email, password] = await register(browser)
+    console.log({ email, password })
 
     // Upload file
 
