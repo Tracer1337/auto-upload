c7beb344fca9da9bbf84134ad24a4cbf5e3c8b4b
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-30 22:23:59 +0200

    Use crypto module to generate files

--- actions/generateFile.js
+++ actions/generateFile.js
@@ -1,48 +1,22 @@
 const fs = require("fs")
 const path = require("path")
 const { v4: uuid } = require("uuid")
+const crypto = require("crypto")
 const config = require("../config.json")
 const { ensureTempDirExists } = require("../utils")
 
+const MEGABYTE = 10 ** 6
 const TEMP_PATH = path.join(__dirname, "..", config.tempDir)
-
-function random(length) {
-    return Math.floor(Math.random() * length)
-}
-
-function writeAsync(writeStream, content) {
-    return new Promise(resolve => {
-        writeStream.write(content, resolve)
-    })
-}
-
-function generateChunk(length, size) {
-    const chunk = []
-    for (let i = 0; i < length; i++) {
-        chunk[i] = random(size)
-    }
-    return chunk
-}
-
-function generateMegabyte() {
-    return generateChunk(10 ** 6, 10 ** 8)
-}
 
 async function generateFile() {
     await ensureTempDirExists()
-    
+
     const filename = uuid()
     const filepath = path.join(TEMP_PATH, filename)
-    const writeStream = fs.createWriteStream(filepath)
 
-    for (let i = 0; i < config.uploadFileSizeMB; i++) {
-        const chunk = generateMegabyte()
-        const buffer = Buffer.from(chunk)
-        await writeAsync(writeStream, buffer)
-    }
-
-    writeStream.close()
-
+    const buffer = crypto.randomBytes(MEGABYTE * config.uploadFileSizeMB)
+    await fs.promises.writeFile(filepath, buffer)
+    
     return filepath
 }
 
